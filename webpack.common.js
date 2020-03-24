838dea7cfa01b0de167444547581d74360f3a888
chengluyu <chengluyu@live.cn>
2020-03-24 09:37:49 +0800

    chore: add tsconfig.json and replace entry point with index.ts

--- webpack.common.js
+++ webpack.common.js
@@ -17,7 +17,7 @@ const styleLoader = {
 }
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, 'src', 'index.ts'),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].[hash].js',
