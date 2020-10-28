e7eb5646953213e73ff7b558121fc04ca8d9e66b
chengluyu <chengluyu@live.cn>
2020-10-28 23:08:02 +0800

    chore(style): config VSCode with Prettier and run format

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserJSPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
 
-const devMode = process.env.NODE_ENV !== 'production';
+const devMode = process.env.NODE_ENV !== "production";
 
 const styleLoader = {
   loader: MiniCssExtractPlugin.loader,
@@ -16,35 +16,37 @@ const styleLoader = {
 };
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.ts'),
+  entry: path.join(__dirname, "src", "index.ts"),
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].[fullhash].js',
+    path: path.join(__dirname, "dist"),
+    filename: "[name].[fullhash].js",
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' },
+      { test: /\.tsx?$/, loader: "ts-loader" },
       {
         test: /\.css$/,
-        use: [styleLoader, 'css-loader'],
+        use: [styleLoader, "css-loader"],
       },
       {
         test: /\.s[ac]ss$/,
-        use: [styleLoader, 'css-loader', 'sass-loader'],
+        use: [styleLoader, "css-loader", "sass-loader"],
       },
-      { test: /\.(?:gif|jpg|png|svg|webp)$/, use: ['file-loader'] },
-      { test: /\.(?:eot|otf|ttf|woff|woff2)$/, use: ['file-loader'] },
+      { test: /\.(?:gif|jpg|png|svg|webp)$/, use: ["file-loader"] },
+      { test: /\.(?:eot|otf|ttf|woff|woff2)$/, use: ["file-loader"] },
     ],
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ template: path.join(__dirname, 'src', 'index.html') }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
     new MiniCssExtractPlugin({
-      filename: devMode ? '[name].css' : '[name].[fullhash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[fullhash].css',
+      filename: devMode ? "[name].css" : "[name].[fullhash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[fullhash].css",
       ignoreOrder: false,
     }),
   ],
